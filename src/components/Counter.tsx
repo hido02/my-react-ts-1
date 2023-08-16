e7971bcf395bb8f52458e282e41e40252f974b45
hido02 <bestdohee@gmail.com>
2023-08-16 09:03:15 +0900

    feat: rtk 카운터 예제 추가

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,21 +1,29 @@
-// src/components/Counter.tsx
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "../actions/CounterAction";
-import { RootState } from "../reducers"; // 수정된 부분
+import React from 'react'
+import type { RootState } from '../store/store'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from '../features/counter/counterSlice'
 
-const Counter: React.FC = () => {
-  const dispatch = useDispatch();
-  const count = useSelector((state: RootState) => state.counter.value);
+export function Counter() {
+  const count = useSelector((state: RootState) => state.counter.value)
+  const dispatch = useDispatch()
 
   return (
     <div>
-      <h2>Counter</h2>
-      <p>Count: {count}</p>
-      <button onClick={() => dispatch(increment())}>Increment</button>
-      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
     </div>
-  );
-};
-
-export default Counter;
+  )
+}
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import todoReducer from '../features/todos/todoSlice';
+import counterReducer from '../features/counter/counterSlice';
 
-const store = configureStore({
+export const store = configureStore({
   reducer: {
+    counter: counterReducer,
     todos: todoReducer,
   },
 });
 
-export default store;
+export type RootState = ReturnType<typeof store.getState>
+
+export type AppDispatch = typeof store.dispatch
